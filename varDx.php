2ccbb78b64a01a377dcfe8a2898a399e84c58a71
Rahul <rahuldottech@users.noreply.github.com>
2018-08-26 16:55:03 +0600

    Create varDx.php

---
+++ varDx.php
@@ -0,0 +1,106 @@
+<?php
+namespace varDX;
+
+/*
+varDX - PHP flat-file storage
+by @rahuldottech
+v1.0
+--
+https://github.com/rahuldottech/
+https://rahul.tech/
+*/
+
+
+class cDX {
+	
+	private $dataFile;	
+	
+	public function def($filename){
+		$this->dataFile = $filename;
+	}
+	
+	public function write($varName, $varVal){
+		if(file_exists($this->dataFile)){
+			$foundLine = $this->check($varName);
+		} else {
+			$foundLine = false;
+		}
+		
+		if(!$foundLine){
+			$writeData = $varName.'__=__'.urlencode($varVal).'__-__'.gettype($varVal).PHP_EOL;
+			file_put_contents($this->dataFile, $writeData, FILE_APPEND);	
+		} else {
+			return "ERR_DX_KEY_ALREADY_EXISTS";
+		}
+	}
+	
+	public function read($varName){
+		if(file_exists($this->dataFile)){
+			$lines_array = file($this->dataFile);
+			$search_string = $varName;
+			foreach($lines_array as $line) {
+				if(strpos($line, $search_string) !== false) {
+					list(, $new_str) = explode("__=__", $line);
+					$foundLine = true;
+				}
+			}
+			if($foundLine){
+				$val = strstr($new_str, '__-__', true); 
+				$type = rtrim(str_replace($val.'__-__', '', $new_str));
+				settype($val, $type);
+				return urldecode($val);
+			} else {
+				return "ERR_DX_KEY_NOT_FOUND";
+			}
+		}
+	}
+
+	
+	public function del($varName){
+		if(file_exists($this->dataFile)){
+		    $f = $this->dataFile;
+			$term = $varName.'__=__';
+			$arr = file($f);
+			foreach ($arr as $key=> $line) {
+				if(stristr($line,$term)!== false){unset($arr[$key]);break;}
+			}
+			//reindexing array
+			$arr = array_values($arr);
+			//writing to file
+			file_put_contents($f, implode($arr));
+		}
+	}
+	
+	public function modify($varName, $varVal){
+		$lines_array = file($this->dataFile);
+		$search_string = $varName;
+		foreach($lines_array as $line) {
+			if(strpos($line, $search_string) !== false) {
+				list(, $new_str) = explode("__=__", $line);
+				$foundLine = true;
+			}
+		}
+		
+		if($foundLine){
+			$this->del($varName);
+		} 
+		
+		$writeData = $varName.'__=__'.urlencode($varVal).'__-__'.gettype($varVal).PHP_EOL;
+		file_put_contents($this->dataFile, $writeData, FILE_APPEND);
+		
+	}
+	
+	public function check($varName){
+		$lines_array = file($this->dataFile);
+		$search_string = $varName;
+		foreach($lines_array as $line) {
+			if(strpos($line, $search_string) !== false) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+}
+	
+
